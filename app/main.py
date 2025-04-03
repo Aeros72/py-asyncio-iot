4c7c070c9ef260c05822f40aa9e196a19a6f51ae
Aeros72 <pashamelnyk2004@gmail.com>
2025-04-04 02:15:39 +0300

    Solution!

--- app/main.py
+++ app/main.py
@@ -32,33 +32,28 @@ async def main() -> None:
         service.register_device(toilet)
     )
 
-    await run_parallel(
-        service.run_program([
-            Message(hue_light_id, MessageType.SWITCH_ON),
-            Message(speaker_id, MessageType.SWITCH_ON)
-        ])
-    )
     await run_sequence(
-        service.run_program(
-            [Message(
-                speaker_id, MessageType.PLAY_SONG,
-                "Rick Astley - Never Gonna Give You Up"
-            )]
+        run_parallel(
+            service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_ON))
+        ),
+        service.send_msg(
+            Message(
+                speaker_id,
+                MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"
+            )
         )
     )
 
-    await run_parallel(
-        service.run_program([
-            Message(hue_light_id, MessageType.SWITCH_OFF),
-            Message(speaker_id, MessageType.SWITCH_OFF)
-        ])
-    )
-
     await run_sequence(
-        service.run_program([
-            Message(toilet_id, MessageType.FLUSH),
-            Message(toilet_id, MessageType.CLEAN)
-        ])
+        run_parallel(
+            service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF))
+        ),
+        run_sequence(
+            service.send_msg(Message(toilet_id, MessageType.FLUSH)),
+            service.send_msg(Message(toilet_id, MessageType.CLEAN))
+        )
     )
 
 
